526b5509b1ff2261e189be4e6bda6fa296165ce3
Jefferson Souza <jefferson.souza@radixeng.com.br>
2024-05-04 16:53:48 -0300

    init repo

---
+++ GiveMeToken.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace GalbenosFuncs
+{
+    public class GiveMeToken
+    {
+        private readonly ILogger<GiveMeToken> _logger;
+
+        public GiveMeToken(ILogger<GiveMeToken> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("GiveMeToken")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            Dictionary<string, string> parametros = new Dictionary<string, string>();
+            foreach (var queryParam in req.Query)
+            {
+                parametros.Add(queryParam.Key, queryParam.Value);
+            }
+
+            foreach (var parametro in parametros)
+            {
+                _logger.LogInformation($"{parametro.Key}: {parametro.Value}");
+            }
+
+            return new OkResult();
+        }
+    }
+}
